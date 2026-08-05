897618706bd23faf6113abc4b4ec2ef9730534ff
pepe-beep <premisconnect@gmail.com>
2026-08-05 21:39:00 +0700

    หลังจาก View Resolver Quiz

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,14 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello from Thymeleaf with a custom ViewResolver!");
+        model.addAttribute("message", " โย่ ปกรณ์เกียรติ ศรีจันทร์");
+        model.addAttribute("studentId", "(รหัส 673380045-2)");
         return "home"; // ไม่ใช่ path ไฟล์ แค่ "ชื่อ view" เชิงตรรกะเท่านั้น
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("about", "สวัสดีครับ มาจาก cs");
+        return "about"; 
     }
 }
